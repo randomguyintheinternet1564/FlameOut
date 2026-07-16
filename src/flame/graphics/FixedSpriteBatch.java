7b732f5711a6dd43d97e039c20172a417fc0aaac
randomguy <tmthien09@gmail.com>
2026-07-16 20:45:24 +0700

    Update FixedSpriteBatch.java

--- src/flame/graphics/FixedSpriteBatch.java
+++ src/flame/graphics/FixedSpriteBatch.java
@@ -33,7 +33,6 @@ public class FixedSpriteBatch extends SpriteBatch{
     }
 
     public void setFixedAlpha(float a){
-        super.setMixColor(a, a, a, a);
     }
     public void setFixedShader(Shader shader){
         super.setShader(shader, true);
@@ -42,10 +41,6 @@ public class FixedSpriteBatch extends SpriteBatch{
         super.setBlending(blending);
     }
 
-    @Override
-    protected void setMixColor(float r, float g, float b, float a){}
-    @Override
-    protected void setMixColor(Color tint){}
     @Override
     protected void setPackedMixColor(float packedColor){}
 
