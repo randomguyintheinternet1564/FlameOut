06df681a3b5ba5ab397549ab847c1f0437a5ec62
randomguy <tmthien09@gmail.com>
2026-07-16 13:58:34 +0700

    Update RicochetAttack.java

--- src/flame/unit/empathy/RicochetAttack.java
+++ src/flame/unit/empathy/RicochetAttack.java
@@ -81,7 +81,7 @@ public class RicochetAttack extends AttackAI{
                     //reload = 2.5f * 60f;
                     reload = 6f * 60f;
 
-                    Sounds.bolt.at(unit.x, unit.y);
+                    Sounds.wind3.at(unit.x, unit.y);
                     unit.randAI(true, unit.health < 50);
                 }
             }
